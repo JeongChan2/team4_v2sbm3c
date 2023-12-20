1c28a6e832dbc89741c0bfa179a5280cd456bf97
Gyuhyeon12 <jtfoo3915@naver.com>
2023-12-20 11:42:57 +0900

    이미지 저장,로드 경로 수정

--- src/main/java/dev/mvc/tool/Tool.java
+++ src/main/java/dev/mvc/tool/Tool.java
@@ -387,10 +387,10 @@ public class Tool {
       String path = "";
       if (File.separator.equals("\\")) {
           // Windows 개발시 사용 폴더
-          // path="C:\\kd\\ws_java\\resort_v2sbm3c\\src\\main\\resources\\static";
+          path="C:/kd/ws_java/team4_v2sbm3c/src/main/resources/static";
           
           // Windows 배포, 폴더 생성
-          path = "C:/kd/deploy/food_v3sbm3c";
+          //path = "C:/kd/deploy/food_v3sbm3c";
 
       } else {
           // Linux 배포
